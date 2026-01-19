72c65b98a23be146fb6aff17b046e1077569bb8f
joao <dev@monitoo.com.br>
2026-01-19 10:10:06 -0300

    polimorfismo de interface em POO

---
+++ secao05/polimorfismo2.py
@@ -0,0 +1,29 @@
+class Forma:
+
+    def atea(self):
+        pass
+
+class Quadrado(Forma):
+
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado ** 2
+    
+class Circulo(Forma):
+
+    def __init__(self, raio):
+        self.raio = raio
+
+    def area(self):
+        return 3.14 * self.raio ** 2
+    
+    
+quadrado = Quadrado(5)
+area_quadrado = quadrado.area()
+print(area_quadrado)
+
+circulo = Circulo(4)
+area_circulo = circulo.area()
+print(area_circulo)
